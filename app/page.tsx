5f314b744d8d6324d65f199385374bee73c89cee
Bill <32452110+bill-7@users.noreply.github.com>
2023-08-25 02:03:37 +0100

    Somewhat working first pass - needs refactoring

--- app/page.tsx
+++ app/page.tsx
@@ -1,7 +1,8 @@
 'use client'
 
 import dynamic from 'next/dynamic'
-import { Suspense } from 'react'
+import Image from 'next/image'
+import { Suspense, useRef, useState } from 'react'
 
 const Blob = dynamic(() => import('@/components/canvas/Examples').then((mod) => mod.Blob), { ssr: false })
 const Dog = dynamic(() => import('@/components/canvas/Examples').then((mod) => mod.Dog), { ssr: false })
@@ -25,14 +26,23 @@ const View = dynamic(() => import('@/components/canvas/View').then((mod) => mod.
 const Common = dynamic(() => import('@/components/canvas/View').then((mod) => mod.Common), { ssr: false })
 
 export default function Page() {
+  const [savedImages, setSavedImages] = useState<string[]>([])
+  const [capture, setCapture] = useState(false)
+  const [dogViewport, setDogViewport] = useState(null)
+
+  const handleSaveImage = (imageUrl: string) => {
+    setCapture(false)
+    setSavedImages((prevImages) => [...prevImages, imageUrl])
+  }
+
   return (
     <>
       <div className='bg-gray-100'>
-        <div className='flex mx-auto w-full flex-col md:flex-row md:w-4/5 md:px-12'>
+        <div className='mx-auto flex w-full flex-col md:w-4/5 md:flex-row md:px-12'>
           <div className='mx-auto flex w-fit items-center'>
             <div className='flex w-fit flex-col items-start px-6 pt-12 text-center md:text-left'>
               <h1 className='text-5xl font-bold leading-tight'>InstaGen</h1>
-              <p className='w-full uppercase text-blue-400 mt-[-10px] mb-2'>A product by Gendo</p>
+              <p className='mb-2 mt-[-10px] w-full uppercase text-blue-400'>A product by Gendo</p>
               <p className='mb-8 text-2xl leading-normal'>Generate and post your own custom images.</p>
             </div>
           </div>
@@ -44,35 +54,45 @@ export default function Page() {
       </div>
 
       <div className='mx-auto flex w-full flex-col flex-wrap items-center px-12 md:flex-row lg:w-4/5'>
-        <div className='relative h-96 w-full py-8 sm:w-1/2'>
+        <div className='dog relative h-96 w-full py-8 sm:w-1/2'>
           <View orbit className='relative h-full sm:w-full'>
             <Suspense fallback={null}>
-              <Dog scale={2} position={[0, -1.6, 0]} rotation={[0.0, -0.3, 0]} />
+              <Dog
+                doCapture={capture}
+                viewport={dogViewport}
+                saveImage={handleSaveImage}
+                scale={2}
+                position={[0, -1.6, 0]}
+                rotation={[0.0, -0.3, 0]}
+              />
               <Common color={'lightblue'} />
             </Suspense>
           </View>
         </div>
-        <div className='relative h-48 w-full py-2 sm:w-1/2 md:my-12 pl-8'>
+        <div className='relative h-48 w-full py-2 pl-8 sm:w-1/2 md:my-12'>
           <h2 className='mb-3 text-3xl font-bold leading-none text-gray-800'>Generate your image!</h2>
           <p className='mb-8 text-gray-600'>
             Drag, scroll, pinch, and rotate the canvas to frame your image, and then when you're happy, hit generate.
           </p>
-          <div className='border border-blue-400 w-fit p-2 rounded-md hover:cursor-pointer hover:bg-blue-50 text-blue-500'>
+          <div
+            className='w-fit rounded-md border border-blue-400 p-2 text-blue-500 hover:cursor-pointer hover:bg-blue-50'
+            onClick={() => {
+              setDogViewport(document.querySelector('.dog').getBoundingClientRect())
+              setCapture(true)
+            }}
+          >
             Generate
           </div>
         </div>
       </div>
       <div className='bg-gray-100 p-8'>
         <div className='mx-auto flex w-full flex-col flex-wrap items-center md:flex-row lg:w-4/5'>
-          <div className='mx-auto grid grid-cols-2 sm:grid-cols-3 md:grid-cols-4 xl:grid-cols-6 gap-4 lg:w-4/5'>
-            {(() => {
-              // TODO!
-              const images = []
-              for (let i = 0; i < 12; i++) {
-                images.push(<div className='w-32 h-32 bg-white rounded-md'></div>)
-              }
-              return images
-            })()}
+          <div className='mx-auto grid grid-cols-2 gap-4 sm:grid-cols-3 md:grid-cols-4 lg:w-4/5 xl:grid-cols-6'>
+            {[...savedImages, ...new Array(12 - savedImages.length).fill(null)].map((image, i) => (
+              <div key={i} className={`h-32 w-32 rounded-md ${image ? 'relative' : 'bg-white'}`}>
+                {image && <Image src={image} alt={`Generated Screenshot ${i}`} width={128} height={128} />}
+              </div>
+            ))}
           </div>
         </div>
       </div>
--- src/components/canvas/Examples.tsx
+++ src/components/canvas/Examples.tsx
@@ -26,12 +26,30 @@ export const Blob = ({ route = '/posts', ...props }) => {
 export function Duck(props) {
   const { scene } = useGLTF('/duck.glb')
 
-  useFrame((state, delta) => (scene.rotation.y += delta))
+  useFrame((delta) => (scene.rotation.y += delta))
 
   return <primitive object={scene} {...props} />
 }
-export function Dog(props) {
-  const { scene } = useGLTF('/dog.glb')
 
+function cropBase64Image(base64, x, y, width, height, saveImage) {
+  const img = new Image()
+  img.src = base64.startsWith('data:image') ? base64 : `data:image/png;base64,${base64}`
+  img.onload = () => {
+    const canvas = document.createElement('canvas')
+    canvas.width = width
+    canvas.height = height
+    canvas.getContext('2d').drawImage(img, x, y, width, height, 0, 0, width, height)
+    saveImage(canvas.toDataURL())
+  }
+}
+
+export function Dog({ doCapture, saveImage, viewport, ...props }) {
+  useFrame(({ gl, scene, camera }) => {
+    gl.render(scene, camera)
+    if (!doCapture) return
+    cropBase64Image(gl.domElement.toDataURL(), viewport.x, viewport.y, viewport.width, viewport.height, saveImage)
+  }, 1)
+
+  const { scene } = useGLTF('/dog.glb')
   return <primitive object={scene} {...props} />
 }
